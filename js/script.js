50c59aceaafe7d8e043fc52be9f68c51b54a9585
Olga Koval <ok@telemark-it.ru>
2019-02-24 13:55:44 +0300

    map js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,66 @@
 (function () {
-/* Popup
-********************************************************************* */
-var popupLinks = document.querySelectorAll(".js-popup-open");
+  /* Popup
+  ********************************************************************* */
+  var popupLinks = document.querySelectorAll(".js-popup-open");
 
-var closePopup = function (popupElem) {
-  document.body.classList.remove("page--popup");
-  popupElem.classList.remove("popup--open");
-  popupElem.classList.add("popup--close");
-};
+  var closePopup = function (popupElem) {
+    document.body.classList.remove("page--popup");
+    popupElem.classList.remove("popup--open");
+    popupElem.classList.add("popup--close");
+  };
 
-var onPopupClose = function (evt) {
-  evt.preventDefault();
-
-  var popupTarget = document.querySelector(".popup--open");
-  closePopup(popupTarget);
-};
-
-var openPopup = function (popupElem) {
-  var closeButton = popupElem.querySelector(".popup__close");
-
-  document.body.classList.add("page--popup");
-  popupElem.classList.remove("popup--close");
-  popupElem.classList.add("popup--open");
-  popupElem.querySelector("input").focus();
-
-  closeButton.addEventListener("click", onPopupClose);
-};
-
-var onPopupLinkClick = function (popupLink) {
-  popupLink.addEventListener("click", function (evt) {
+  var onPopupClose = function (evt) {
     evt.preventDefault();
-    var popupTarget = document.querySelector(this.getAttribute("href"));
-    openPopup(popupTarget);
-  });
-};
 
-[].forEach.call(popupLinks, onPopupLinkClick);
+    var popupTarget = document.querySelector(".popup--open");
+    closePopup(popupTarget);
+  };
+
+  var openPopup = function (popupElem) {
+    var closeButton = popupElem.querySelector(".popup__close");
+
+    document.body.classList.add("page--popup");
+    popupElem.classList.remove("popup--close");
+    popupElem.classList.add("popup--open");
+    popupElem.querySelector("input").focus();
+
+    closeButton.addEventListener("click", onPopupClose);
+  };
+
+  var onPopupLinkClick = function (popupLink) {
+    popupLink.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      var popupTarget = document.querySelector(this.getAttribute("href"));
+      openPopup(popupTarget);
+    });
+  };
+
+  [].forEach.call(popupLinks, onPopupLinkClick);
+
+  /* Map
+  ********************************************************************* */
+  var mapContainer = document.querySelector("#map");
+
+  if (mapContainer) {
+    ymaps.ready(function () {
+      var map = new ymaps.Map("map", {
+        center: [59.939, 30.3215],
+        zoom: 17,
+        scrollZoom: false,
+        controls: []
+      }, {
+        searchControlProvider: "yandex#search"
+      }),
+      Placemark = new ymaps.Placemark([59.938652, 30.323201], {
+        balloonContent: "Санкт-Петербург, Большая Конюшенная улица, 19/8"
+      }, {
+        iconLayout: "default#image",
+        iconImageHref: "img/pin.png",
+        iconImageSize: [231, 190],
+        iconImageOffset: [-49, -190]
+      });
+
+      map.geoObjects.add(Placemark);
+    });
+  }
 })();
